b30d8a65b4474b52d1f31d281ac720828fe11097
finalight <finalight@gmail.com>
2018-05-02 21:36:10 +0800

    initial commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+
+    def index
+        render :json => {:lol => "get all"}
+    end
+
+    def create()
+        json_request = JSON.parse(request.body.read())
+        from = json_request["message"]['from']['id']
+        message = json_request['message']['text']
+
+        options = {
+            body: {
+              chat_id: from,
+              text: message
+            }
+          }
+      
+        response = HTTParty.post("https://api.telegram.org/bot" + ENV["TELEGRAM_API_KEY] + "/sendMessage", options)
+        puts response.body
+    end
+
+    def get
+        render :json => {:lol => "hello"}
+    end
+end
