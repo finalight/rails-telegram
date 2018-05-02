07569f09415a4bb35359558a203ae53007fa06d1
finalight <finalight@gmail.com>
2018-05-02 21:58:08 +0800

    add exception

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,19 +5,24 @@ class MessagesController < ApplicationController
     end
 
     def create()
-        json_request = JSON.parse(request.body.read())
-        from = json_request["message"]['from']['id']
-        message = json_request['message']['text']
+        begin
+            json_request = JSON.parse(request.body.read())
+            from = json_request["message"]['from']['id']
+            message = json_request['message']['text']
 
-        options = {
-            body: {
-              chat_id: from,
-              text: message
+            options = {
+                body: {
+                chat_id: from,
+                text: message
+                }
             }
-          }
-      
-        response = HTTParty.post("https://api.telegram.org/bot" + ENV["TELEGRAM_API_KEY"] + "/sendMessage", options)
-        puts response.body
+        
+            response = HTTParty.post("https://api.telegram.org/bot" + ENV["TELEGRAM_API_KEY"] + "/sendMessage", options)
+            puts response.body
+        rescue Exception => e
+            puts e.message
+        end
+
     end
 
     def get
